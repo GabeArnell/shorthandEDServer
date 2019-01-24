523a5039cd4d4961c0aac611028170f1ecae28e2
GabeArnell <43541642+GabeArnell@users.noreply.github.com>
2019-01-24 15:15:15 -0500

    added server basic json serving

--- node-test-server/app.js
+++ node-test-server/app.js
@@ -5,38 +5,50 @@ var path = require('path');
 /*
 node C:\Users\gabrielarnell\node-test-server\app.js
 */
+
+var nestedClassDataForStudent = {
+	classid: 1,
+	emojis:[],
+};
+
+var testStudentAlphaArray = {
+	name:"Albert",
+	id:1,
+	classes:[nestedClassDataForStudent]
+};
+
 var server = http.createServer(function(req, res){
   console.log('Request was made: ' + req.url);
+  //API Check
+  if (req.url === '/api.json'){
+    console.log('Serving API');
+    res.writeHead(200,{'Content-Type': 'application/json'});
+    res.end(JSON.stringify(testStudentAlphaArray));
+  };
+  // Returning static web pages/files
 
   let filePath = __dirname+'/public'+req.url;
-
   switch (req.url) {
     case "/":
-      console.log('Returning index page')
       res.writeHead(200,{'Content-Type': 'text/html'});
       fs.createReadStream(__dirname + '/public/index.html').pipe(res);
       break
     case "/favicon.ico":
-      console.log("SEARCHING FOR FAVICON");
       res.writeHead(200,{'Content-Type': 'image/x-icon'});
       fs.createReadStream(__dirname + '/public/favicon.ico').pipe(res);
       break
     default:
-      console.log('searching for filepath: ' + filePath);
 
       if (fs.existsSync(filePath)) {
-        console.log('Found File');
         // TEXT FILES
         if(path.extname(filePath) === '.css' || path.extname(filePath) === '.html' || path.extname(filePath) === '.js'){
           let fileExtension = path.extname(filePath).slice(1,path.extname(filePath).length);
-          console.log('Extension:'+fileExtension);
           res.writeHead(200,{'Content-Type': 'text/'+fileExtension});
           fs.createReadStream(filePath).pipe(res);
         }
         //Images
         if(path.extname(filePath) === '.gif' || path.extname(filePath) === '.png' || path.extname(filePath) === '.jpg'){
           let fileExtension = path.extname(filePath).slice(1,path.extname(filePath).length);
-          console.log('Extension:'+fileExtension);
           res.writeHead(200,{'Content-Type': 'image/'+fileExtension});
           fs.createReadStream(filePath).pipe(res);
         }
@@ -50,4 +62,21 @@ var server = http.createServer(function(req, res){
 
 
 server.listen(3000, '127.0.0.1');
-console.log('now listening to port 3000');
+
+console.log('Opened port: 3000');
+
+// Accessing mysql server
+
+ /* var mysql = require('mysql');
+
+ var con = mysql.createConnection({
+	 user: "root",
+	 password: "Every time I access this I cry",
+	 database: 'testdatabase'
+ });
+
+con.connect(function(err){
+	 if (err) throw err;
+	 console.log('hAcKeR voIcE: iM In!')
+});
+*/
